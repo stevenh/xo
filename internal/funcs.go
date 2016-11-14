d0dadbd326b977e6dc0d4d6173899ebe67dd4f8a
Steven Hartland <steven.hartland@multiplay.co.uk>
2016-11-14 11:12:57 +0000

    Added fieldtag helper
    Added a fieldtag template helper which creates a fully constructed
    field tag.

--- internal/funcs.go
+++ internal/funcs.go
@@ -25,6 +25,7 @@ func (a *ArgType) NewTemplateFuncs() template.FuncMap {
 		"fieldnameslist":     a.fieldnameslist,
 		"fieldnamesslice":    a.fieldnamesslice,
 		"fieldnames":         a.fieldnames,
+		"fieldtag":           a.fieldtag,
 		"goparamlist":        a.goparamlist,
 		"goparam":            a.goparam,
 		"reniltype":          a.reniltype,
@@ -236,6 +237,22 @@ func (a *ArgType) colnames(fields []*Field, ignoreNames ...string) string {
 	}
 
 	return str
+}
+
+// fieldtag returns the fieldtag for the field f.
+func (a *ArgType) fieldtag(f *Field) string {
+	parts := []string{f.Col.ColumnName}
+	if f.Col.IsAutoIncrement {
+		parts = append(parts, "autoinc")
+	}
+	if f.Col.IsPrimaryKey {
+		parts = append(parts, "pk")
+	}
+	if f.Col.DefaultValue.Valid {
+		parts = append(parts, "default")
+	}
+
+	return strings.Join(parts, ",")
 }
 
 // fieldnameslist returns the field names for fields.
